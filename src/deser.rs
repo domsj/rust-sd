87dfefc04a5d485cf974b4f74d2c578555a4b6ec
Jan Doms <jan.doms@gmail.com>
2015-08-17 23:15:33 +0200

    a bit of refactoring (extract deser.rs)

---
+++ src/deser.rs
@@ -0,0 +1,125 @@
+use std::option::Option;
+use std::io::prelude::*;
+use std::io;
+use std::error;
+use std::result;
+use std::vec::*;
+use std::fmt;
+use std::fmt::{Formatter, Display};
+
+extern crate byteorder;
+use self::byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+
+#[derive(Debug)]
+pub enum Error {
+    ParseError,
+    ByteorderError(byteorder::Error),
+    IoError(io::Error)
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match *self {
+            Error::ParseError => write!(f, "Parse error"),
+            Error::IoError(ref err) => write!(f, "IO error: {}", err),
+            Error::ByteorderError(ref err) => write!(f, "ByteorderError: {}", err),
+        }
+    }
+}
+impl From<io::Error> for Error {
+    fn from(err : io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+impl From<byteorder::Error> for Error {
+    fn from(err : byteorder::Error) -> Error {
+        Error::ByteorderError(err)
+    }
+}
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::ParseError => "parse error!",
+            Error::IoError(ref err) => error::Error::description(err),
+            Error::ByteorderError(ref err) => byteorder::Error::description(err),
+        }
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
+pub fn read_bytes_raw(reader: &mut Read, size : usize) ->
+    Result<Vec<u8>> {
+
+    // hmm, there's got to be a better way
+    let mut res = Vec::<u8>::with_capacity(size);
+    unsafe { res.set_len(size) }
+
+    let mut read = 0;
+    while read < size {
+        println!("cuc1 {} {}", read, size);
+        let read_extra = try!(reader.read(&mut res[read..]));
+        println!("{}", read_extra);
+        read += read_extra;
+    };
+    Ok(res)
+}
+
+pub fn read_u32(reader : &mut Read) -> Result<u32> {
+    Ok(try!(reader.read_u32::<LittleEndian>()))
+}
+
+pub fn read_u8(reader : &mut Read) -> Result<u8> {
+    Ok(try!(reader.read_u8()))
+}
+
+pub fn read_bytes(reader: &mut Read) -> Result<Vec<u8>> {
+    let len = { try!(reader.read_u32::<LittleEndian>()) };
+    read_bytes_raw(reader, len as usize)
+}
+
+pub fn read_bool(reader : &mut Read) -> Result<bool> {
+    match try!(reader.read_u8()) {
+        0 => Ok(false),
+        1 => Ok(true),
+        _ => Err(Error::ParseError)
+    }
+}
+
+pub fn read_option<F, T>(reader: &mut Read, read_t: F)
+               -> Result<Option<T>>
+    where F : Fn(&mut Read) -> Result<T> {
+    if try!(read_bool(reader)) {
+        Ok(Some(try!(read_t(reader))))
+    } else {
+        Ok(None)
+    }
+}
+
+pub fn read_bytes_option(reader : &mut Read) -> Result<Option<Vec<u8>>> {
+    read_option(reader, read_bytes)
+}
+
+fn read_list<F, T>(reader: &mut Read, read_t: F)
+                   -> Result<Vec<T>>
+    where F : Fn(&mut Read) -> Result<T> {
+    let len = reader.read_u32::<LittleEndian>().unwrap();
+    let mut res = Vec::new();
+    for _ in 0..len {
+        res.push(try!(read_t(reader)))
+    }
+    Ok(res)    
+}
+
+pub fn read_bytes_list(reader : &mut Read) -> Result<Vec<Vec<u8>>> {
+    read_list(reader, read_bytes)
+}
+
+pub fn write_u32(reader : &mut Write, i : u32) -> Result<()> {
+    Ok(try!(reader.write_u32::<LittleEndian>(i)))
+}
+
+pub fn write_bytes(writer : &mut Write, bytes : &[u8]) {
+    writer.write_u32::<LittleEndian>(bytes.len() as u32).unwrap();
+    writer.write_all(bytes).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -9,128 +9,26 @@ use std::io::prelude::*;
 use std::io::{Cursor};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::vec::*;
 use std::sync::{Arc};
 
 extern crate rocksdb;
 use rocksdb::{RocksDB, RocksDBResult, Writable, WriteBatch};
 
-extern crate byteorder;
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
-
-#[derive(Debug)]
-enum Error {
-    ParseError,
-    ByteorderError(byteorder::Error),
-    IoError(std::io::Error)
-}
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Error::ParseError => write!(f, "Parse error"),
-            Error::IoError(ref err) => write!(f, "IO error: {}", err),
-            Error::ByteorderError(ref err) => write!(f, "ByteorderError: {}", err),
-        }
-    }
-}
-impl From<std::io::Error> for Error {
-    fn from(err : std::io::Error) -> Error {
-        Error::IoError(err)
-    }
-}
-impl From<byteorder::Error> for Error {
-    fn from(err : byteorder::Error) -> Error {
-        Error::ByteorderError(err)
-    }
-}
-impl std::error::Error for Error {
-    fn description(&self) -> &str {
-        match *self {
-            Error::ParseError => "parse error!",
-            Error::IoError(ref err) => std::error::Error::description(err),
-            Error::ByteorderError(ref err) => byteorder::Error::description(err),
-        }
-    }
-}
-type Result<T> = std::result::Result<T, Error>;
-
-fn read_bytes_raw(reader : &mut Read, size : usize) ->
-    Result<Vec<u8>> {
-
-    // hmm, there's got to be a better way
-    let mut res = Vec::<u8>::with_capacity(size);
-    unsafe { res.set_len(size) }
-
-    let mut read = 0;
-    while read < size {
-        println!("cuc1 {} {}", read, size);
-        let read_extra = try!(reader.read(&mut res[read..]));
-        println!("{}", read_extra);
-        read += read_extra;
-    };
-    Ok(res)
-}
-
-fn read_bytes(reader : &mut Read) -> Result<Vec<u8>> {
-    let len = try!(reader.read_u32::<LittleEndian>());
-    read_bytes_raw(reader, len as usize)
-}
-
-fn read_bool(reader : &mut Read) -> Result<bool> {
-    // TODO return result
-    match try!(reader.read_u8()) {
-        0 => Ok(false),
-        1 => Ok(true),
-        _ => Err(Error::ParseError)
-    }
-}
-
-fn read_option<F, T>(reader: &mut Read, read_t: F)
-               -> Result<Option<T>>
-    where F : Fn(&mut Read) -> Result<T> {
-    if try!(read_bool(reader)) {
-        Ok(Some(try!(read_t(reader))))
-    } else {
-        Ok(None)
-    }
-
-}
-fn read_bytes_option(reader : &mut Read) -> Result<Option<Vec<u8>>> {
-    read_option(reader, read_bytes)
-}
-
-fn read_list<F, T>(reader: &mut Read, read_t: F)
-                   -> Result<Vec<T>>
-    where F : Fn(&mut Read) -> Result<T> {
-    let len = reader.read_u32::<LittleEndian>().unwrap();
-    let mut res = Vec::new();
-    for _ in 0..len {
-        res.push(try!(read_t(reader)))
-    }
-    Ok(res)    
-}
-
-fn read_bytes_list(reader : &mut Read) -> Result<Vec<Vec<u8>>> {
-    read_list(reader, read_bytes)
-}
-
-fn write_bytes(writer : &mut Write, bytes : &[u8]) {
-    writer.write_u32::<LittleEndian>(bytes.len() as u32).unwrap();
-    writer.write_all(bytes).unwrap();
-}
+mod deser;
+use deser::*;
 
 fn prologue(mut stream: &mut TcpStream) -> Result<()> {
     println!("1");
     let magic = b"aLbA";
-    let magic1 = read_bytes_raw(&mut stream, 4).ok().unwrap();
+    let magic1 = try!(deser::read_bytes_raw(stream, 4));
     assert!(magic1 == magic);
 
     println!("2");
-    let version = stream.read_u32::<LittleEndian>().unwrap();
+    let version = try!(deser::read_u32(stream));
     assert!(version == 1);
 
     println!("3");
-    let lido = try!(read_bytes_option(&mut stream));
+    let lido = try!(deser::read_bytes_option(stream));
 
     let long_id = b"the_hardcoded_id";
 
@@ -145,8 +43,8 @@ fn prologue(mut stream: &mut TcpStream) -> Result<()> {
             }
     };
 
-    stream.write_u32::<LittleEndian>(0).unwrap();
-    write_bytes(stream, long_id);
+    deser::write_u32(stream, 0).unwrap();
+    deser::write_bytes(stream, long_id);
     stream.flush().unwrap();
 
     println!("5");
@@ -157,11 +55,13 @@ enum AsdError {
     UnknownOperation = 4
 }
 
-fn reply_unknown(mut stream : &mut TcpStream) {
+fn reply_unknown(mut stream : &mut TcpStream) -> Result<()> {
     println!("replying unknown!");
-    stream.write_u32::<LittleEndian>(4).unwrap();
-    stream.write_u32::<LittleEndian>(AsdError::UnknownOperation as u32).unwrap();
-    stream.flush().unwrap();
+    try!(deser::write_u32(stream, 4));
+    try!(deser::write_u32(stream,
+                          AsdError::UnknownOperation as u32));
+    try!(stream.flush());
+    Ok(())
 }
 
 #[macro_use] extern crate enum_primitive;
@@ -175,6 +75,29 @@ enum_from_primitive! {
     }
 }
 
+fn handle_multiget(mut stream: &mut TcpStream,
+                   mut cur: &mut Read,
+                   db: &RocksDB) -> Result<()> {
+    let keys = try!(deser::read_bytes_list(&mut cur));
+    let mut res : Vec<Option<_>> = Vec::new();
+    // let db = db.snapshot();
+    // TODO snapshot meegeven in readoptions
+    // voor de get call (needs changes in the
+    // rocksdb binding...
+    // yak shaving here I come?
+
+    // also maybe provide a non atomic
+    // multiget variant
+    for key in keys {
+        res.push(match db.get(&key) {
+            RocksDBResult::Error(e) => panic!(e),
+            RocksDBResult::None => Option::None,
+            RocksDBResult::Some(s) => Option::Some(s)
+        });
+    }
+    reply_unknown(&mut stream)
+}
+
 fn handle_client(mut stream: TcpStream, db: Arc<RocksDB>) -> Result<()> {
     // TODO wrap TcpStream with io::BufReader
 
@@ -183,28 +106,21 @@ fn handle_client(mut stream: TcpStream, db: Arc<RocksDB>) -> Result<()> {
     println!("6");
 
     loop {
-        let msg = try!(read_bytes(&mut stream));
+        let msg = try!(deser::read_bytes(&mut stream));
         let mut cur = Cursor::new(msg);
         println!("7");
-        match Operation::from_u8(cur.read_u8().unwrap()) {
+        let op = Operation::from_u8(try!(deser::read_u8(&mut cur)));
+        try!(match op {
             None => reply_unknown(&mut stream),
             Some(operation) =>
                 match operation {
-                    Operation::MultiGet => {
-                        let keys = try!(read_bytes_list(&mut cur));
-                        let mut res = Vec::new();
-                        for key in keys {
-                            res.push(match db.get(&key) {
-                                RocksDBResult::Error(e) => panic!(e),
-                                RocksDBResult::None => Option::None,
-                                RocksDBResult::Some(s) => Option::Some(s)
-                            });
-                        }
-                        reply_unknown(&mut stream)
-                    },
+                    Operation::MultiGet =>
+                        handle_multiget(&mut stream,
+                                        &mut cur,
+                                        &*db),
                     Operation::Range => reply_unknown(&mut stream)
                 }
-        };
+        });
         let _ = db.put(b"my key", b"my key");
         db.get(b"my key");
         let batch = WriteBatch::new();
